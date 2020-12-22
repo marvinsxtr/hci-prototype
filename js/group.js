800d32a27c87a6d94418ba0da19d23df32d39e7c
Til Goldan <til.goldan@googlemail.com>
2020-12-22 18:37:22 +0100

    Generate Groups from JS

--- js/group.js
+++ js/group.js
@@ -1,3 +1,22 @@
+let groups = [
+  {
+    "name": "Stadtpuff",
+    "members": "Leonard, Marvin, Jansan, Til, Andre",
+    "icon": "../assets/image.jpeg"
+  },
+  {
+    "name": "?????",
+    "members": "Nils, Ruwen, Kalobi, Ilkay, Kafe",
+    "icon": "../assets/group2.jpeg"
+  },
+  {
+    "name": "Group XY",
+    "members": "Lea, Nele, Gerry",
+    "icon": "../assets/group3.jpeg"
+  }
+]
+
+
 $("ul.buttonGroup").click(function (event) {
     var item= $("li", this)
     .removeClass("selected")
@@ -29,4 +48,33 @@ const navSlide = () => {
 }
 
 
-navSlide();
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  navSlide();
+
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  
+  selected_group = -1
+
+  if (urlParams.has("group_id"))
+    selected_group = urlParams.get("group_id")
+
+  groups.forEach((g, i) => {
+    console.log(selected_group)
+    console.log(i)
+    $("#group-selector").append(`
+      <a  href="group.html?group_id=${i}" class="group-box ${selected_group == i ? "group-selected": ""}"> 
+        <div class="circle">
+          <img src="${g.icon}">
+        </div>
+        <div class="group">
+          <p class="groupname">${g.name}</p>
+          <p class="members">${g.members}</p>
+        </div>
+      </a>
+    `)
+  })
+  
+});
